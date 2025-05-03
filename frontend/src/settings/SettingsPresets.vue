8711f3f75684107d501a42a6cf77165c86920f8e
Perfect03 <zlobin_gg52926@mail.ru>
2025-05-04 02:06:05 +0500

    chore: remove @element-plus/icons dependencie; some refactor presets logic

--- frontend/src/settings/SettingsPresets.vue
+++ frontend/src/settings/SettingsPresets.vue
@@ -19,26 +19,30 @@ const createdPresetInput = ref(null)
 const creating = ref(false)
 
 const create = () => {
+  creating.value = true
+  createdPreset.value = JSON.parse(selectedPreset.value).name
+  setTimeout(() => {
+    createdPresetInput.value?.select()
+  }, 300)
+}
+
+const confirm = () => {
   createdPresetError.value = false
-  if (creating.value) {
-    if (createdPreset.value.length) {
-      emit('create', createdPreset.value)
-      creating.value = false
-      setTimeout(() => {
-        const newPreset = (props.presets || []).find(el => el.name == createdPreset.value)
-        if (newPreset) selectedPreset.value = JSON.stringify(newPreset)
-        createdPreset.value = ''
-      }, 300)
-    }
-    else createdPresetError.value = true
-  }
-  else {
-    creating.value = true
-    createdPreset.value = JSON.parse(selectedPreset.value).name
+  if (createdPreset.value.length) {
+    emit('create', createdPreset.value)
+    creating.value = false
     setTimeout(() => {
-      createdPresetInput.value?.select()
-    }, 300)
+      const newPreset = (props.presets || []).find(el => el.name == createdPreset.value)
+      if (newPreset) selectedPreset.value = JSON.stringify(newPreset)
+      createdPreset.value = ''
+    }, 250)
   }
+  else createdPresetError.value = true
+}
+
+const cancel = () => {
+  createdPresetError.value = false
+  creating.value = false
 }
 
 function select(pres: Settings) {
@@ -67,28 +71,31 @@ function deletePreset(preset: Settings) {
             @confirm="deletePreset(preset)" :title="t('settings.generalSettings.presetDelete')">
             <template #reference v-if="preset.name">
               <el-icon :size="14" style="margin-right: -1em; margin-left: 1em" :title="t('settings.generalSettings.presetDelete')" @click.stop="">
-                <Delete />
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1024 1024" data-v-d2e47025=""><path fill="currentColor" d="M160 256H96a32 32 0 0 1 0-64h256V95.936a32 32 0 0 1 32-32h256a32 32 0 0 1 32 32V192h256a32 32 0 1 1 0 64h-64v672a32 32 0 0 1-32 32H192a32 32 0 0 1-32-32zm448-64v-64H416v64zM224 896h576V256H224zm192-128a32 32 0 0 1-32-32V416a32 32 0 0 1 64 0v320a32 32 0 0 1-32 32m192 0a32 32 0 0 1-32-32V416a32 32 0 0 1 64 0v320a32 32 0 0 1-32 32"></path></svg>
               </el-icon>
             </template>
           </el-popconfirm>
         </div>
         </el-option>
       </el-select>
-      <el-input v-model="createdPreset" @blur="creating = false" @keyup.enter="create" ref="createdPresetInput" class="presets__create" :class="{ 'is-invalid': createdPresetError && !createdPreset.length }" :placeholder="t('settings.generalSettings.presetInputPlaceholder')" tabindex="-1" />
+      <el-input v-model="createdPreset" @keyup.enter="create" ref="createdPresetInput" class="presets__create" :class="{ 'is-invalid': createdPresetError && !createdPreset.length }" :placeholder="t('settings.generalSettings.presetInputPlaceholder')" tabindex="-1" />
     </div>
-    <div class="settings-item">
-      <el-icon :size="creating ? 13 : 20" style="cursor: pointer" :title="t(`settings.generalSettings.preset${creating ? 'Accept' : 'Create'}`)">
-        <div v-if="creating" @mousedown="create">
-          <svg :style="!createdPreset.length && {'pointer-events': none}" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M9.54998 18.0001L3.84998 12.3001L5.27498 10.8751L9.54998 15.1501L18.725 5.9751L20.15 7.4001L9.54998 18.0001Z" fill="black"/>
-          </svg>
-        </div>
-        <div v-else @click="create">
-          <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M15.2 3C15.7275 3.00751 16.2307 3.22317 16.6 3.6L20.4 7.4C20.7768 7.76926 20.9925 8.27246 21 8.8V19C21 19.5304 20.7893 20.0391 20.4142 20.4142C20.0391 20.7893 19.5304 21 19 21H5C4.46957 21 3.96086 20.7893 3.58579 20.4142C3.21071 20.0391 3 19.5304 3 19V5C3 4.46957 3.21071 3.96086 3.58579 3.58579C3.96086 3.21071 4.46957 3 5 3H15.2Z" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-            <path d="M17 21V14C17 13.7348 16.8946 13.4804 16.7071 13.2929C16.5196 13.1054 16.2652 13 16 13H8C7.73478 13 7.48043 13.1054 7.29289 13.2929C7.10536 13.4804 7 13.7348 7 14V21M7 3V7C7 7.26522 7.10536 7.51957 7.29289 7.70711C7.48043 7.89464 7.73478 8 8 8H15" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-          </svg>
-        </div>
+    <div class="settings-item" v-if="creating">
+      <el-icon @click="confirm" :size="13" style="cursor: pointer" :title="t('settings.generalSettings.presetAccept')">
+        <svg :style="!createdPreset.length && {'pointer-events': none}" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M9.54998 18.0001L3.84998 12.3001L5.27498 10.8751L9.54998 15.1501L18.725 5.9751L20.15 7.4001L9.54998 18.0001Z" fill="black"/>
+        </svg>
+      </el-icon>
+      <el-icon @click="cancel" :size="13" style="cursor: pointer" :title="t('settings.generalSettings.presetCancel')">
+        <svg :style="!createdPreset.length && {'pointer-events': none}" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1024 1024" data-v-d2e47025=""><path fill="currentColor" d="M764.288 214.592 512 466.88 259.712 214.592a31.936 31.936 0 0 0-45.12 45.12L466.752 512 214.528 764.224a31.936 31.936 0 1 0 45.12 45.184L512 557.184l252.288 252.288a31.936 31.936 0 0 0 45.12-45.12L557.12 512.064l252.288-252.352a31.936 31.936 0 1 0-45.12-45.184z"></path></svg>
+      </el-icon>
+    </div>
+    <div class="settings-item" v-else @click="create">
+      <el-icon @click="create" :size="20" style="cursor: pointer" :title="t('settings.generalSettings.presetCreate')">
+        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M15.2 3C15.7275 3.00751 16.2307 3.22317 16.6 3.6L20.4 7.4C20.7768 7.76926 20.9925 8.27246 21 8.8V19C21 19.5304 20.7893 20.0391 20.4142 20.4142C20.0391 20.7893 19.5304 21 19 21H5C4.46957 21 3.96086 20.7893 3.58579 20.4142C3.21071 20.0391 3 19.5304 3 19V5C3 4.46957 3.21071 3.96086 3.58579 3.58579C3.96086 3.21071 4.46957 3 5 3H15.2Z" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+          <path d="M17 21V14C17 13.7348 16.8946 13.4804 16.7071 13.2929C16.5196 13.1054 16.2652 13 16 13H8C7.73478 13 7.48043 13.1054 7.29289 13.2929C7.10536 13.4804 7 13.7348 7 14V21M7 3V7C7 7.26522 7.10536 7.51957 7.29289 7.70711C7.48043 7.89464 7.73478 8 8 8H15" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+        </svg>
       </el-icon>
     </div>
   </div>
